a8ba541e2eab10970824c9be1cf461b937aade6f
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-04-09 04:32:58 +0530

    Create Hello world.cpp

---
+++ Hello world.cpp
@@ -0,0 +1,13 @@
+Try [this](https://codeforces.com/problemset/problem/4/A) problem.
+
+Following is a starting template you may use:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  cout << n+1 << " " << "Watermelon" << endl;
+  return 0;
+}
