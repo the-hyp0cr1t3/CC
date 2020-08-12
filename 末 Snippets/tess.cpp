3396b6e6a0971aa81e356ad3c93819f9ccc23bc2
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-08-12 19:05:18 +0530

    Add files via upload

---
+++ 末 Snippets/tess.cpp
@@ -0,0 +1,27 @@
+int indeg[N];
+vector<int> g[N];
+
+for(i = 0; i < m; i++) {
+    int u, v;
+    cin >> u >> v;
+    u--, v--;
+    g[u].pb(v);
+    indeg[v]++;
+}
+
+auto topsort = [&]() {
+    queue<int> q;
+    vector<int> order; order.reserve(n);
+    for(i = 0; i < n; i++) 
+        if(!indeg[i]) q.push(i);
+
+    while(!q.empty()) {
+        auto v = q.front();
+        q.pop();
+        order.pb(v);
+        for(auto& x: g[v]) 
+            if(!--indeg[x]) q.push(x);
+    }
+
+    return sz(order) == n;      // cycle? false : true
+};
