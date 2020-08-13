c7df76df46b13a9b4440104181cf24455fbe6938
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-08-14 04:49:06 +0530

    Update Segment tree.cpp

--- 末 Snippets/Segment tree.cpp
+++ 末 Snippets/Segment tree.cpp
@@ -1,184 +1,114 @@
 // Inspired by [Ashishgup](https://github.com/Ashishgup1/Competitive-Coding)
-struct Data {
-    int val;
-    Data() : val(0) {}
 
-    void init(int idx) {
-        this->val = a[idx-1];
-    }
-
-    void merge(const Data& l, const Data& r) {
-        this->val = min(l.val, r.val);
-    }
-
-    void updateval(int val) {
-        this->val = val;
-    }
-
-};
-
+template<class T>
 struct SegTree {
-    int N;
-    vector<Data> st;
-
+    int N; vector<T> st;
+    SegTree(int N) : N(N), st(vector<T>(4*N+5)) {}
+    
     void build(int node, int L, int R) {
-        if(L == R) {
-            st[node].init(L);
-            return;
-        }
-        int M = (L+R)/2;
-        build(node*2, L, M);
-        build(node*2 + 1, M + 1, R);
-        st[node].merge(st[node*2], st[node*2+1]);
+        if(L == R) return st[node] <<= L;
+        int M = L+R>>1;
+        build(node*2, L, M); build(node*2+1, M+1, R);
+        st[node] = T(st[node*2], st[node*2+1]);
     }
 
-    SegTree(int n) : N(n) {
-        st.rsz(4*n + 5);
-    }
-
-    Data Query(int node, int L, int R, int i, int j) {
-        if(i > R or j < L)
-            return Data();
-        if(i <= L and j >= R)
-            return st[node];
-        int M = (L + R)/2;
-        Data left = Query(node*2, L, M, i, j);
-        Data right = Query(node*2 + 1, M + 1, R, i, j);
-        Data cur; cur.merge(left, right);
-        return cur;
+    T Query(int node, int L, int R, int i, int j) {
+        if(i > R or j < L) return T();
+        if(i <= L and j >= R) return st[node];
+        int M = L+R>>1;
+        return T(Query(node*2, L, M, i, j), Query(node*2+1, M+1, R, i, j));
     } 
 
     void Update(int node, int L, int R, int i, int j, int val) {
-        if(i > R or j < L)
-            return;
-        if(i <= L and j >= R) {
-            st[node].updateval(val);
-            return;
-        }
-        int M = (L + R)/2;
+        if(i > R or j < L) return;
+        if(i <= L and j >= R) return st[node] << val;
+        int M = L+R>>1;
         Update(node*2, L, M, i, j, val);
-        Update(node*2 + 1, M + 1, R, i, j, val);
-        st[node].merge(st[node*2], st[node*2 + 1]);
+        Update(node*2+1, M+1, R, i, j, val);
+        st[node] = T(st[node*2], st[node*2+1]);
     }
 
-    Data query(int pos) {
-        return Query(1, 1, N, pos, pos);
-    }
-
-    Data query(int l, int r) {
-        return Query(1, 1, N, l, r);
-    }
-
-    void update(int pos, int val) {
-        Update(1, 1, N, pos, pos, val);
-    }
-
-    void update(int l, int r, int val) {
-        Update(1, 1, N, l, r, val);
-    }
-
+    T query(int pos) { return Query(1, 1, N, pos, pos); }
+    T query(int l, int r) { return Query(1, 1, N, l, r); }
+    void update(int pos, int val) { Update(1, 1, N, pos, pos, val); }
+    void update(int l, int r, int val) { Update(1, 1, N, l, r, val); }
 };
+
+struct Node {
+    int val;
+    Node(int val = 0) : val(val) {}    
+    Node(const Node& l, const Node& r) {
+        val = min(l.val, r.val);
+    }
+    void operator<<=(int idx) { val = a[idx-1]; }
+    void operator<<(int delta) { val += delta; }
+};
+
 
 /* -------------------------------------------------- */
 // With lazy prop
-struct Data {
-    int val;
-    Data() : val(0) {}
 
-    void init(int idx) {
-        this->val = a[idx-1];
-    }
-
-    void merge(const Data& l, const Data& r) {
-        this->val = min(l.val, r.val);
-    }
-
-    void lazymerge(int delta) {
-        this->val += delta;
-    }
-};
-
+template<class T, class U = int>
 struct SegTree {
-    int N;
-    vector<Data> st;
-    vector<bool> cLazy;
-    vector<int> lazy;
+    int N; vector<T> st; vector<U> lazy; vector<bool> pending;
+    SegTree(int N): N(N), st(vector<T>(4*N+5)), 
+        lazy(vector<U>(4*N+5)), pending(vector<bool>(4*N+5)) {}
 
     void build(int node, int L, int R) {
-        if(L == R) {
-            st[node].init(L);
-            return;
-        }
-        int M = (L+R)/2;
-        build(node*2, L, M);
-        build(node*2 + 1, M + 1, R);
-        st[node].merge(st[node*2], st[node*2+1]);
-    }
-
-    SegTree(int n) : N(n) {
-        st.rsz(4*n + 5);
-        cLazy.assign(4*n + 5, false);
-        lazy.assign(4*n + 5, 0);
+        if(L == R) return st[node] <<= L;
+        int M = L+R>>1;
+        build(node*2, L, M); build(node*2+1, M+1, R);
+        st[node] = T(st[node*2], st[node*2+1]);
     }
     
-    void propagate(int node, int L, int R) {
+    void prop(int node, int L, int R) {
         if(L != R) {
-            cLazy[node*2] = 1;
-            cLazy[node*2 + 1] = 1;
+            pending[node*2] = pending[node*2+1] = true;
             lazy[node*2] += lazy[node];
-            lazy[node*2 + 1] += lazy[node]; 
+            lazy[node*2+1] += lazy[node]; 
         }
-        st[node].lazymerge(lazy[node]);
-        cLazy[node] = 0;
-        lazy[node] = 0;
+        st[node] << lazy[node];
+        pending[node] = false;
+        lazy[node] = U();
     }
 
-    Data Query(int node, int L, int R, int i, int j) {
-        if(cLazy[node])
-            propagate(node, L, R);
-        if(i > R or j < L)
-            return Data();
-        if(i <= L and j >= R)
-            return st[node];
-        int M = (L + R)/2;
-        Data left = Query(node*2, L, M, i, j);
-        Data right = Query(node*2 + 1, M + 1, R, i, j);
-        Data cur; cur.merge(left, right);
-        return cur;
-    } 
+    T Query(int node, int L, int R, int i, int j) {
+        if(pending[node]) prop(node, L, R);
+        if(i > R or j < L) return T();
+        if(i <= L and j >= R) return st[node];
+        int M = L+R>>1;
+        return T(Query(node*2, L, M, i, j), Query(node*2+1, M+1, R, i, j));
+    }
 
     void Update(int node, int L, int R, int i, int j, int val) {
-        if(cLazy[node])
-            propagate(node, L, R);
-        if(i > R or j < L)
-            return;
-        if(i <= L and j >= R) {
-            cLazy[node] = 1;
-            lazy[node] = val;
-            propagate(node, L, R);
-            return;
-        }
-        int M = (L + R)/2;
+        if(pending[node]) prop(node, L, R);
+        if(i > R or j < L) return;
+        if(i <= L and j >= R) return lazy[node] += val, prop(node, L, R);
+        int M = L+R>>1;
         Update(node*2, L, M, i, j, val);
-        Update(node*2 + 1, M + 1, R, i, j, val);
-        st[node].merge(st[node*2], st[node*2 + 1]);
+        Update(node*2+1, M+1, R, i, j, val);
+        st[node] = T(st[node*2], st[node*2+1]);
     }
 
-    Data query(int pos) {
-        return Query(1, 1, N, pos, pos);
-    }
-
-    Data query(int l, int r) {
-        return Query(1, 1, N, l, r);
-    }
-
-    void update(int pos, int val) {
-        Update(1, 1, N, pos, pos, val);
-    }
-
-    void update(int l, int r, int val) {
-        Update(1, 1, N, l, r, val);
-    }
-
+    T query(int pos) { return Query(1, 1, N, pos, pos); }
+    T query(int l, int r) { return Query(1, 1, N, l, r); }
+    void update(int pos, int val) { Update(1, 1, N, pos, pos, val); }
+    void update(int l, int r, int val) { Update(1, 1, N, l, r, val); }
 };
 
+struct Lazy {
+    int val;
+    Lazy(int val = 0): val(val) {}    
+    void operator+=(const int& rhs) { val = rhs; }
+    void operator+=(const Lazy& rhs) { val = rhs.val; }
+};
+
+struct Node {
+    int val;
+    Node(int val = 0): val(val) {}    
+    Node(const Node& l, const Node& r) {
+        val = max(l.val, r.val);
+    }
+    void operator<<=(int idx) { val = a[idx-1]; }
+    void operator<<(const Lazy& lzy) { val += lzy.val; }
+};
