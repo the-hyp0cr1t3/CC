d23308196249cabc07d6a827a844557968513e94
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-08-15 21:57:14 +0530

    Update DSU.cpp

--- 末 Snippets/DSU.cpp
+++ 末 Snippets/DSU.cpp
@@ -1,71 +1,25 @@
-// short implementation
-vector<int> par(n), sz_(n, 1);
-iota(all(par), 0);
-
-auto getpar = [&] (int x) {
-    int pp = x;
-    while(par[pp] != pp)
-        pp = par[pp];
-    while(par[x] != x) {
-        int z = par[x];
-        par[x] = pp;
-        x = z;
-    }
-    return pp;        
-};
-
-auto merge = [&] (int x, int y) {
-    x = getpar(x);
-    y = getpar(y);
-    if (x == y) return false;
-    if (sz_[x] < sz_[y]) swap(x, y);
-    par[y] = x, sz_[x] += sz_[y];
-    return true;
-};
-
-/* -------------------------------------------------- */
-// DSU struct
 struct DSU {
-    int n, par[N], size[N], roots[N], rootID[N], rootcnt;
-    DSU(int Q) : n(Q), rootcnt(Q) {
-        iota(par, par+n, 0);
-        fill(size, size+n, 1);
-    }
-
-    int getpar(int x) {
-        int pp = x;
-        while(par[pp] != pp)
-            pp = par[pp];
-        while(par[x] != x) {
-            int z = par[x];
-            par[x] = pp;
-            x = z;
-        }
-        return pp;
-    }
-
+    int components;
+    vector<int> data, rootID, rootnode; 
+    DSU(int n): n(n), components(n), data(vector<int>(n+1, -1)), 
+        rootID(vector<int>(n+1, -1)) {}
+    int par(int x) { return data[x] < 0 ? x : data[x] = par(data[x]); }
+    int SZ(int x) { return -data[par(x)]; }
     bool merge(int x, int y) {
-        x = getpar(x);
-        y = getpar(y);
-        if (x == y) return false;
-        if (size[x] < size[y]) swap(x, y);
-        par[y] = x, size[x] += size[y];
-        rootcnt--;
+        x = par(x); y = par(y);
+        if(x == y) return false;
+        if(-data[x] < -data[y]) swap(x, y);
+        data[x] += data[y]; data[y] = x;
+        components--;
         return true;
     }
-
-    bool same(int x, int y) {
-        return getpar(x) == getpar(y);
-    }
-    
     void getRoots() {
         int cnt = 0;
         for(int i = 0; i < n; i++) {
-            if (par[i] != i) continue;
-            rootID[i] = cnt;
-            roots[cnt++] = i;
+            if(data[i] >= 0) continue;
+            rootID[i] = cnt++;
+            roots.pb(i);
         }
-        assert(cnt == rootcnt);
+        assert(cnt == components);
     }
 };
-
