beb89e24b31578a5ea724b5f16a13e4a7bc0cc82
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-09-13 22:25:45 +0530

    Update pbds.cpp

--- 末 Snippets/pbds.cpp
+++ 末 Snippets/pbds.cpp
@@ -1,8 +1,7 @@
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
-#define ook order_of_key
-#define fbo find_by_order
 using namespace __gnu_pbds;
-template <typename T>
-using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-
+template<class key, class value = null_type, class cmp = std::less<key>>
+using ordered_set = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
+#define ook order_of_key    // count of elements strictly smaller than k
+#define fbo find_by_order   // iterator to kth element starting from 0
