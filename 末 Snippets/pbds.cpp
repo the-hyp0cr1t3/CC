b7ce39f4bf625cc39066313e89c0bff6f0c8ee34
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-09-24 17:26:41 +0530

    Update pbds.cpp

--- 末 Snippets/pbds.cpp
+++ 末 Snippets/pbds.cpp
@@ -5,3 +5,5 @@ template<class key, class value = null_type, class cmp = std::less<key>>
 using ordered_set = tree<key, value, cmp, rb_tree_tag, tree_order_statistics_node_update>;
 #define ook order_of_key    // count of elements strictly smaller than k
 #define fbo find_by_order   // iterator to kth element starting from 0
+
+ordered_set<int> os;
