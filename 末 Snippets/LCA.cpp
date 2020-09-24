74ef5421cc8d238a0ce917d48bc3ac87b6567243
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-09-24 16:56:08 +0530

    Update LCA.cpp

--- 末 Snippets/LCA.cpp
+++ 末 Snippets/LCA.cpp
@@ -42,85 +42,90 @@ int LCA(int u, int v) {
 /* -------------------------------------------------- */
 // LCA Sparse Table
 // O(N) precomp, O(1) per query, O(N) space
+int n;
+vector<int> g[N];   // adjacency list
+
 int tin[N], d[N], lg2[2*N];
 vector<int> tree, bmask;
 vector<vector<int>> sparse;
 vector<vector<vector<int>>> best;
-
-int len = 2*n-1, j, k; log2[0] = -1;
-for(i = 1; i <= len; i++) 
-    log2[i] = log2[i/2] + 1;
-
-int b = max(1, log2[len]/2);
-int cntb = (len+b-1)/b;
-
-bmask.resize(cntb); best.resize(1<<b-1);
-sparse.resize(log2[cntb]+1, vector<int>(cntb));
-tree.reserve(len);
+int len, b, cntb;
 
 void dfs(int v, int p) {
-    tin[v] = sz(tree);
-    tree.pb(v);
+    tin[v] = sz(tree); tree.pb(v);
     for(auto x: g[v]) { 
         if(x == p) continue;
         d[x] = d[v] + 1;
-        dfs(x, v);
-        tree.pb(v);
-    }    
-}
-
-dfs(0, -1);
-
-auto lower = [&](int p, int q) {
-    return d[tree[p]] < d[tree[q]]? p : q;
-};
-
-for(i = 0; i < len; i++) {
-    int curb = i/b; j = i%b;
-    if(!j or lower(sparse[0][curb], i) == i)
-        sparse[0][curb] = i;
-    if(j and lower(i-1, i) == i-1)
-        bmask[curb] |= 1<<j-1;
-}
-
-for(k = 1; k <= log2[cntb]; k++) {
-    for(i = 0; i < cntb; i++) {
-        j = i + (1<<k-1);
-        sparse[k][i] = sparse[k-1][i];
-        if(j < cntb)
-            sparse[k][i] = lower(sparse[k-1][i], sparse[k-1][j]);
+        dfs(x, v); tree.pb(v);
     }
 }
 
-for(int curb = 0; curb < cntb; curb++) {
-    int mask = bmask[curb];
-    if(sz(best[mask])) continue;
-    best[mask].resize(b, vector<int>(b));
-    for(i = 0; i < b; i++) {
-        best[mask][i][i] = i;
-        for(j = i+1; j < b; j++) {
-            if(b*curb+j >= len) break;
-            best[mask][i][j] = lower(b*curb+best[mask][i][j-1], b*curb+j);
-            best[mask][i][j] -= b*curb;
+bool lower(int p, int q) {
+    return d[tree[p]] < d[tree[q]]? p : q;
+}
+
+void precomp() {
+    int i, j, k;
+    len = 2*n-1; lg2[0] = -1;
+    for(i = 1; i <= len; i++) 
+        lg2[i] = lg2[i/2] + 1;
+
+    b = max(1, lg2[len]/2);
+    cntb = (len+b-1)/b;
+
+    bmask.resize(cntb); best.resize(1<<b-1);
+    sparse.resize(lg2[cntb]+1, vector<int>(cntb));
+    tree.reserve(len);
+
+    dfs(0, -1);
+
+    for(i = 0; i < len; i++) {
+        int curb = i/b; j = i%b;
+        if(!j or lower(sparse[0][curb], i) == i)
+            sparse[0][curb] = i;
+        if(j and lower(i-1, i) == i-1)
+            bmask[curb] |= 1<<j-1;
+    }
+
+    for(k = 1; k <= lg2[cntb]; k++) {
+        for(i = 0; i < cntb; i++) {
+            j = i + (1<<k-1);
+            sparse[k][i] = sparse[k-1][i];
+            if(j < cntb)
+                sparse[k][i] = lower(sparse[k-1][i], sparse[k-1][j]);
+        }
+    }
+
+    for(int curb = 0; curb < cntb; curb++) {
+        int mask = bmask[curb];
+        if(sz(best[mask])) continue;
+        best[mask].resize(b, vector<int>(b));
+        for(i = 0; i < b; i++) {
+            best[mask][i][i] = i;
+            for(j = i+1; j < b; j++) {
+                if(b*curb+j >= len) break;
+                best[mask][i][j] = lower(b*curb+best[mask][i][j-1], b*curb+j);
+                best[mask][i][j] -= b*curb;
+            }
         }
     }
 }
 
-auto geti = [&](int curb, int l, int r) {
+int geti(int curb, int l, int r) {
     return best[bmask[curb]][l][r] + b*curb;
-};
+}
 
-auto LCA = [&](int u, int v) {
-    u = tin[u];
-    v = tin[v];
+int LCA(int u, int v) {
+    u = tin[u]; v = tin[v];
     if(u > v) swap(u, v);
     int bu = u/b, bv = v/b;
     if(bu == bv)
         return tree[geti(bu, u%b, v%b)];
     int res = lower(geti(bu, u%b, b-1), geti(bv, 0, v%b));
     if(bv-bu > 1) {
-        int z = log2[bv-bu-1];
+        int z = lg2[bv-bu-1];
         res = lower(res, lower(sparse[z][bu+1], sparse[z][bv-(1<<z)]));
-    }
-    return tree[res];
-};
+    } return tree[res];
+}
+
+// precomp(); int lca = LCA(u, v);
