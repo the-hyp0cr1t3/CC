f7a3e47efd0d356b65ecdd4e0c107b0156686cfb
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-09-24 18:02:42 +0530

    Update Segment tree.cpp

--- 末 Snippets/Segment tree.cpp
+++ 末 Snippets/Segment tree.cpp
@@ -1,48 +1,50 @@
 template<class T>
 struct Segtree {
     int N; vector<T> st;
-    Segtree(int N): N(N) {
-        int SZ = 1; for(SZ = 1; SZ < N; SZ <<= 1);
-        st.resize(2*SZ+2);
-    }
-    template<class Iter> Segtree(Iter beg, Iter end): N(end-beg) {
-        int SZ = 1; for(SZ = 1; SZ < N; SZ <<= 1);
-        st.resize(2*SZ+2); build(1, 1, N, beg, end);
-    }
+    Segtree(int N)
+        : N(N) { int SZ = 1; for(SZ = 1; SZ < N; SZ <<= 1); st.resize(2*SZ+2); }
+    template<class Iter> 
+    Segtree(Iter beg, Iter end)
+        : Segtree(end-beg) { build(1, 1, N, beg, end); }
     
-    template<class Iter> void build(int node, int L, int R, Iter beg, Iter end) {
+    template<class Iter>
+    void build(int node, int L, int R, Iter beg, Iter end) {
         if(L == R) return st[node].set(*beg);
-        int M = L+R>>1;
-        build(node*2, L, M, beg, beg+(M-L)); build(node*2+1, M+1, R, beg+(M-L+1), end);
-        st[node] = T(st[node*2], st[node*2+1]);
+        int M = L + R >> 1;
+        build(node<<1, L, M, beg, beg+(M-L));
+        build(node<<1|1, M+1, R, beg+(M-L+1), end);
+        st[node] = T(st[node<<1], st[node<<1|1]);
     }
 
     T Query(int node, int L, int R, int i, int j) {
         if(i > R or j < L) return T();
         if(i <= L and j >= R) return st[node];
-        int M = L+R>>1;
-        return T(Query(node*2, L, M, i, j), Query(node*2+1, M+1, R, i, j));
+        int M = L + R >> 1;
+        return T(Query(node<<1, L, M, i, j), 
+                    Query(node<<1|1, M+1, R, i, j));
     } 
 
     void Update(int node, int L, int R, int pos, int val) {
         if(L == R) return st[node].upd(val);
-        int M = L+R>>1;
-        pos <= M? Update(node*2, L, M, pos, val) : Update(node*2+1, M+1, R, pos, val);
-        st[node] = T(st[node*2], st[node*2+1]);
+        int M = L + R >> 1;
+        pos <= M? Update(node<<1, L, M, pos, val)
+                    : Update(node<<1|1, M+1, R, pos, val);
+        st[node] = T(st[node<<1], st[node<<1|1]);
     }
 
+    // helper/wrapper funcs
     auto query(int pos) { return Query(1, 1, N, pos, pos); }
     auto query(int l, int r) { return Query(1, 1, N, l, r); }
     void update(int pos, int val) { Update(1, 1, N, pos, val); }
 };
 
 struct Node {
-    int val;
-    Node(int val = 0) : val(val) {}    
-    Node(const Node& l, const Node& r)
+    int val{0};     // brace initialize all default values
+    Node() = default;
+    Node(const Node& l, const Node& r)      // merge two nodes
         : val(l.val + r.val) {}
-    void set(int init) { val = init; }
-    void upd(int delta) { val += delta; }
+    void set(int init) { val = init; }      // set value during build()
+    void upd(int delta) { val += delta; }   // update value
 };
 
 /*
