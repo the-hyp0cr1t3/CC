f22d59b445ab7ee3ae92304ac0ba338ddf8db9ef
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-09-24 16:29:22 +0530

    Update Edge_t.cpp

--- 末 Snippets/Edge_t.cpp
+++ 末 Snippets/Edge_t.cpp
@@ -1,5 +1,6 @@
 struct edge_t {
     int u, v, w;
+    edge_t() = default;
     edge_t(int u, int v, int w) : u(u), v(v), w(w) {}
     bool operator<(const edge_t& other) { return w < other.w; }
     friend ostream& operator<<(ostream& os, const edge_t& e) { os << e.u << ' ' << e.v << ' ' << e.w; return os; }
