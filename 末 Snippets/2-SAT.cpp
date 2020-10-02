88e07dff403a06677469b3507cac01a06541a855
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-10-02 17:15:26 +0530

    Update 2-SAT.cpp

--- 末 Snippets/2-SAT.cpp
+++ 末 Snippets/2-SAT.cpp
@@ -1,11 +1,25 @@
-int vis[2*N], comp[2*N], assignment[N], n;
+// (https://codeforces.com/contest/776/submission/94484305)
+
+int vis[2*N], comp[2*N], assignment[N];
 vector<int> g[2*N], rg[2*N], order;
 
-// (p ∨ q) implication edge
+// (p ∨ q) meaning at least one is true
 void addedge(int p, int q) {
 // (¬p → q) ∧ (¬q → p)
     g[p^1].pb(q); g[q^1].pb(p);
     rg[q].pb(p^1); rg[p].pb(q^1);
+}
+
+// (p ⊕ q) meaning exactly one is true
+void xoredge(int p, int q) {
+// (p ∨ q) ∧ (¬p ∨ ¬q)
+    addedge(p, q); addedge(p^1, q^1);
+}
+
+// (p ↔ q) both are true or both are false
+void biconditionaledge(int p, int q) {
+// (p → q) ∧ (q → p) ≡ (p ⊕ ¬q)
+    xoredge(p, q^1);
 }
 
 void dfs1(int v) {
@@ -22,7 +36,7 @@ void dfs2(int v, int k) {
 }
 
 // i is represented by 2*i, ¬i is represented by 2*i+1
-bool is_sat() {
+bool is_sat(int n) {
     memset(comp, -1, sizeof(comp));
     order.reserve(2*n);
     for(int i = 0; i < 2*n; i++)
