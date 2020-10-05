679474545136bdb35fc610ce02f89ceee9622428
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-10-05 16:24:45 +0530

    Update DSU.cpp

--- 末 Snippets/DSU.cpp
+++ 末 Snippets/DSU.cpp
@@ -1,25 +1,31 @@
 struct DSU {
     int n, components;
     vector<int> data, rootID, roots; 
-    DSU(int n): n(n), components(n), data(vector<int>(n+1, -1)), 
-        rootID(vector<int>(n+1, -1)) {}
-    int par(int x) { return data[x] < 0 ? x : data[x] = par(data[x]); }
-    int SZ(int x) { return -data[par(x)]; }
+    
+    DSU(int n): n(n), components(n), data(n+1, -1), rootID(n+1, -1) {}
+    
+    int par(int x) { 
+        return data[x] < 0 ? x : data[x] = par(data[x]); 
+    }
+    
+    int SZ(int x) { 
+        return -data[par(x)]; 
+    }
+    
     bool merge(int x, int y) {
         x = par(x); y = par(y);
         if(x == y) return false;
         if(-data[x] < -data[y]) swap(x, y);
-        data[x] += data[y]; data[y] = x;
-        components--;
+        data[x] += data[y]; data[y] = x; components--;
         return true;
     }
+    
     void getRoots() {
         int cnt = 0;
         for(int i = 0; i < n; i++) {
             if(data[i] >= 0) continue;
             rootID[i] = cnt++;
             roots.pb(i);
-        }
-        assert(cnt == components);
+        } assert(cnt == components);
     }
 };
