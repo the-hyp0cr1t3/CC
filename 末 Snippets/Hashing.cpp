426feb737e20e037c98bcfada0b9e9175683050b
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-10-06 18:47:50 +0530

    Update Hashing.cpp

--- 末 Snippets/Hashing.cpp
+++ 末 Snippets/Hashing.cpp
@@ -56,23 +56,3 @@ struct custom_hash {
         return splitmix64(x + FIXED_RANDOM);
     }
 };
-
-// ------------------------------------------------------------------------
-// gp_hash_table, a better alternative to std::unordered_map (https://codeforces.com/blog/entry/60737)
-#include <ext/pb_ds/assoc_container.hpp>
-__gnu_pbds::gp_hash_table<int, int> table;
-
-// write a custom hash for different keys, something like
-struct custom_hash {
-    static uint64_t splitmix64(uint64_t x) {
-        x += 0x9e3779b97f4a7c15;
-        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
-        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
-        return x ^ (x >> 31);
-    }
-    size_t operator()(const pair<uint64_t, uint64_t>& x) const {
-        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x.second + FIXED_RANDOM) ^ (splitmix64(FIXED_RANDOM + x.first) << 1);
-    }
-};
-__gnu_pbds::gp_hash_table<pair<int, int>, int, custom_hash> table;
