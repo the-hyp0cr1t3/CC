55268e39d7e5390e0bc6586982e2487593cab52f
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-10-21 02:35:46 +0530

    Update Dijkstra.cpp

--- 末 Snippets/Dijkstra.cpp
+++ 末 Snippets/Dijkstra.cpp
@@ -8,20 +8,25 @@ for(int i = 0; i < m; i++) {
 }
 
 auto dijkstra = [&] (int root) {
-    vector<int64_t> d(n, INF);
-    vector<int> par(n, -1);
     struct state {
         int v; int64_t dist;
         state(int v, int64_t dist): v(v), dist(dist) {}
-        bool operator<(const state& o) { return dist > o.dist; }
-    }; priority_queue<state> pq;
+        bool operator<(const state& o) const { 
+            return dist > o.dist; 
+        }
+    }; 
+    
+    vector<int64_t> d(n, INF);
+    vector<int> par(n, -1);
+    priority_queue<state> pq;
     for(d[root] = i = 0; i < n; i++)
         pq.emplace(i, d[i]);
+    
     while(!pq.empty()) {
         state top = pq.top(); pq.pop();
         if(top.dist > d[top.v]) continue;
-        for(auto& [to, w]: g[v])
-            if(chmin(d[to], curd + w))
-                pq.emplace(to, d[to]), par[to] = v;
+        for(auto& [to, w]: g[top.v])
+            if(chmin(d[to], top.dist + w))
+                pq.emplace(to, d[to]), par[to] = top.v;
     }
 };
