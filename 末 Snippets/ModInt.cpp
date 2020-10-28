0f3937bed5822d074835708535b6d518018953c0
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2020-10-28 16:58:31 +0530

    Update ModInt.cpp

--- 末 Snippets/ModInt.cpp
+++ 末 Snippets/ModInt.cpp
@@ -1,37 +1,64 @@
-// Inspired by [ecnerwala](https://codeforces.com/profile/ecnerwala)
-const int MOD = 1e9+7;
+// Template by [neal](https://codeforces.com/profile/neal)
 
-struct num {
-    typedef decay<decltype(MOD)>::type T;
-    T v; using ll = int64_t;
-    static T inv(T a, T m) { a %= m; return a == 1 ? 1 : (T)(m - ll(inv(m, a)) * ll(m) / a); } 
-    num() : v(0) {}
-    num(ll v_) {
-        v = (-MOD < v_ && v_ < MOD) ? T(v_) : T(v_ % MOD);
-        if (v < 0) v += MOD;
+template<const int& MOD>
+struct Mint {
+    using T = decay<decltype(MOD)>::type; T v;
+    Mint(int64_t v = 0) { if(v < 0) v = v % MOD + MOD; if(v >= MOD) v %= MOD; this->v = T(v); }
+    Mint(uint64_t v) { if (v >= MOD) v %= MOD; this->v = T(v); }
+    Mint(int v): Mint(int64_t(v)) {}
+    Mint(unsigned v): Mint(uint64_t(v)) {}
+    explicit operator int() const { return v; }
+    explicit operator int64_t() const { return v; }
+    explicit operator uint64_t() const { return v; }
+    friend istream& operator>>(istream& in, Mint& m) { int64_t v_; in >> v_; m = Mint(v_); return in; } 
+    friend ostream& operator<<(ostream& os, const Mint& m) { return os << m.v; }
+
+    static T inv(T a, T m) {
+        T g = m, x = 0, y = 1;
+        while(a != 0) {
+            T q = g / a;
+            g %= a; swap(g, a);
+            x -= q * y; swap(x, y);
+        } return x < 0? x + m : x;
     }
-    explicit operator T() const { return v; }
-    friend std::ostream& operator << (std::ostream& out, const num& n) { return out << T(n); }
-    friend std::istream& operator >> (std::istream& in, num& n) { ll v_; in >> v_; n = num(v_); return in; } 
-    friend bool operator == (const num& a, const num& b) { return a.v == b.v; }
-    friend bool operator != (const num& a, const num& b) { return a.v != b.v; }
-    num inv() const { num res; res.v = inv(v, MOD); return res; }
-    num operator- () const { num res; res.v = v ? MOD-v : 0; return res; }
-    num operator+ () const { return num(*this); }
-    num& operator ++ () { v ++; if (v == MOD) v = 0; return *this; }
-    num& operator -- () { if (v == 0) v = MOD; v --; return *this; }
-    num& operator += (const num& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
-    num& operator -= (const num& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
-    num& operator *= (const num& o) { v = T(ll(v) * ll(o.v) % MOD); return *this; }
-    num& operator /= (const num& o) { return *this *= o.inv(); }
-    friend num operator ++ (num& a, int) { num r = a; ++a; return r; }
-    friend num operator -- (num& a, int) { num r = a; --a; return r; }
-    friend num operator + (const num& a, const num& b) { return num(a) += b; }
-    friend num operator - (const num& a, const num& b) { return num(a) -= b; }
-    friend num operator * (const num& a, const num& b) { return num(a) *= b; }
-    friend num operator / (const num& a, const num& b) { return num(a) /= b; }
-    friend bool operator < (const num& a, const num& b) { return a.v < b.v; }
-    friend bool operator <= (const num& a, const num& b) { return a.v <= b.v; }
-    friend bool operator > (const num& a, const num& b) { return a.v > b.v; }
-    friend bool operator >= (const num& a, const num& b) { return a.v >= b.v; }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif // x must be less than 2^32 * m
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x), quot, rem;
+        asm("divl %4\n" : "=a" (quot), "=d" (rem) : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    Mint inv() const { return Mint(inv(v, MOD)); }
+    Mint operator-() const { return Mint(v? MOD-v : 0); }
+    Mint& operator++() { v++; if(v == MOD) v = 0; return *this; }
+    Mint& operator--() { if(v == 0) v = MOD; v--; return *this; }
+    Mint operator++(int) { Mint a = *this; ++*this; return a; }
+    Mint operator--(int) { Mint a = *this; --*this; return a; }
+    Mint& operator+=(const Mint& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    Mint& operator-=(const Mint& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    Mint& operator*=(const Mint& o) { v = fast_mod(uint64_t(v) * o.v); return *this; }
+    Mint& operator/=(const Mint& o) { return *this *= o.inv(); }
+    friend Mint operator+(const Mint& a, const Mint& b) { return Mint(a) += b; }
+    friend Mint operator-(const Mint& a, const Mint& b) { return Mint(a) -= b; }
+    friend Mint operator*(const Mint& a, const Mint& b) { return Mint(a) *= b; }
+    friend Mint operator/(const Mint& a, const Mint& b) { return Mint(a) /= b; }
+    friend bool operator==(const Mint& a, const Mint& b) { return a.v == b.v; }
+    friend bool operator!=(const Mint& a, const Mint& b) { return a.v != b.v; }
+    friend bool operator<(const Mint& a, const Mint& b) { return a.v < b.v; }
+    friend bool operator>(const Mint& a, const Mint& b) { return a.v > b.v; }
+    friend bool operator<=(const Mint& a, const Mint& b) { return a.v <= b.v; }
+    friend bool operator>=(const Mint& a, const Mint& b) { return a.v >= b.v; }
+    Mint operator^(int64_t p) {
+        if(p < 0) return inv() ^ -p;
+        Mint a = *this, res{1}; while(p > 0) {
+            if(p & 1) res *= a;
+            p >>= 1; if(p > 0) a *= a;
+        } return res;
+    }
 };
+
+const int MOD = 1e9+7;
+using mint = Mint<MOD>;
