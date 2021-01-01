86ff2e607fcc8a9fa5cc9ceb5335ce90fb5f3ca0
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 00:05:10 +0530

    Update Binary exponentiation.cpp

--- 末 Snippets/Binary exponentiation.cpp
+++ 末 Snippets/Binary exponentiation.cpp
@@ -1,5 +1,6 @@
+// (https://cp-algorithms.com/algebra/binary-exp.html)
 template<typename T>
-T expo(T A, ll B) {
+T expo(T A, int64_t B) {        // A^{B}
     T res{1}; while(B) {
         if(B & 1) res = res * A;
         B >>= 1; A = A * A;
