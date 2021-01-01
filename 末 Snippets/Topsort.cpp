d3e6e4f26b9a41724fe460e3309e22f0878e7a28
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 02:49:23 +0530

    Update Topsort.cpp

--- 末 Snippets/Topsort.cpp
+++ 末 Snippets/Topsort.cpp
@@ -1,25 +1,23 @@
+/* Topological sort */
+
 int indeg[N];
 vector<int> g[N];
 
 for(i = 0; i < m; i++) {
-    int u, v;
-    cin >> u >> v;
-    u--, v--;
-    g[u].pb(v);
+    int u, v; cin >> u >> v; u--, v--;
+    g[u].push_back(v);
     indeg[v]++;
 }
 
 auto topsort = [&]() {
-    queue<int> q;
-    vector<int> order; order.reserve(n);
+    queue<int> q; vector<int> order;
+    order.reserve(n);
     for(i = 0; i < n; i++) 
         if(!indeg[i]) q.push(i);
     while(!q.empty()) {
-        auto v = q.front();
-        q.pop();
-        order.pb(v);
+        auto v = q.front(); q.pop();
+        order.push_back(v);
         for(auto& x: g[v]) 
             if(!--indeg[x]) q.push(x);
-    }
-    return sz(order) == n;      // cycle? false : true
+    } return sz(order) == n;      // cycle? false : true
 };
