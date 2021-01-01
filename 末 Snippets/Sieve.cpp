47f22025e45c0f342706feb71a03da37e4baa135
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 02:47:04 +0530

    Update Sieve.cpp

--- 末 Snippets/Sieve.cpp
+++ 末 Snippets/Sieve.cpp
@@ -1,32 +1,13 @@
-vector<int> primes;
-vector<int> spf;
-vector<bool> prime;
+/* Sieve of Eratosthenes */
+// (https://cp-algorithms.com/algebra/sieve-of-eratosthenes.html)
 
-auto sieve = [&] (int maxn) {
-    primes.reserve(maxn/100);
-    prime.assign(maxn, true);
-    spf.assign(maxn, 0);
-    prime[0] = prime[1] = false;
-
-    primes.pb(2);
-    spf[2] = 2;
-    for(i = 4; i < maxn; i+=2)
-        prime[i] = false, spf[i] = 2;
-
-    for(i = 3; i*i < maxn; i+=2) {
-        if(prime[i]) {
-            for(int j = i*i; j < maxn; j+=i*2) {
-                if(prime[j]) 
-                    prime[j] = false, spf[j] = i;
-            }
-        }
-    }
-
-    for(i = 3; i < maxn; i+=2) {
-        if(prime[i]) {
-            primes.pb(i);
-            spf[i] = i;
-        }
-    }
-};
-
+const int N = 1e7;
+// spf = smallest prime factor
+array<int, N> spf; iota(all(spf), 0);
+vector<int> primes{2}; primes.reserve(N);
+for(int z = 4; z < N; z += 2) spf[z] = 2;
+for(int z = 3; z*z < N; z += 2)
+    if(spf[z] == z) for(int y = z*z; y < N; y += z*2)
+        if(spf[y] == y) spf[y] = z;
+for(int z = 3; z < N; z += 2) 
+    if(spf[z] == z) primes.pb(z);
