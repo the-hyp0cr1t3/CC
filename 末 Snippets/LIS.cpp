6ea92c9eae60a41a38da42423075a7a3d1697c21
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 00:31:39 +0530

    Update LIS.cpp

--- 末 Snippets/LIS.cpp
+++ 末 Snippets/LIS.cpp
@@ -1,35 +1,33 @@
-auto LIS = [&] (vector<int>& v) {
+auto LIS = [&] (const vector<int>& v) {
     vector<int> lis; lis.reserve(n);
-    for(i = 0; i < n; i++) {
+    for(int z = 0; z < n; z++) {
         // v[A] <= v[B] for strictly incr, v[A] < v[B] otherwise
-        auto it = upper_bound(all(lis), i, [&](const auto& A, const auto& B) { 
+        auto it = upper_bound(all(lis), z, [&](const auto& A, const auto& B) { 
             return v[A] <= v[B];
         });
         int idx = int(it - lis.begin());
-        if(it == lis.end()) lis.pb(i);
-        else lis[idx] = i;
-    }
-    return sz(lis);
+        if(it == lis.end()) lis.pb(z);
+        else lis[idx] = z;
+    } return sz(lis);           // returns length
 };
 
 /* -------------------------------------------------- */
 // With path
-auto LIS = [&] (vector<int>& v) {
+auto LIS = [&] (const vector<int>& v) {
     vector<int> lis; lis.reserve(n);
     vector<int> path(n, -1), res; res.reserve(n);
-    for(i = 0; i < n; i++) {
+    for(int z = 0; z < n; z++) {
         // v[A] <= v[B] for strictly incr, v[A] < v[B] otherwise
-        auto it = upper_bound(all(lis), i, [&](const auto& A, const auto& B) { 
+        auto it = upper_bound(all(lis), z, [&](const auto& A, const auto& B) { 
             return v[A] <= v[B];
         });
         int idx = int(it - lis.begin());
-        if(it == lis.end()) lis.pb(i);
-        else lis[idx] = i;
-        if(idx) path[i] = lis[idx-1];
+        if(it == lis.end()) lis.pb(z);
+        else lis[idx] = z;
+        if(idx) path[z] = lis[idx-1];
     }
     int cur = lis.back();
     while(~cur) res.pb(cur), cur = path[cur];
-    reverse(all(res));
-    return res;
+    return reverse(all(res)), res;          // returns vector of indices
 };
 
