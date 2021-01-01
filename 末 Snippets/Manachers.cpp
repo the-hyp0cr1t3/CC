aa10e5a9621efb8d74a9872a22ba66f2bf200bc6
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 00:49:32 +0530

    Update Manachers.cpp

--- 末 Snippets/Manachers.cpp
+++ 末 Snippets/Manachers.cpp
@@ -1,27 +1,24 @@
-auto manachers = [&] (vector<int>& odd, vector<int>& even) {
-// a b c b a a b b
-// 1 1 3 1 1 1 1 1
-// 0 0 0 0 0 2 0 1
-    odd.resize(n);
-    even.resize(n);
-    int l = 0, r = -1;
-    for(i = 0; i < n; i++) {
-        int k = (i > r) ? 1 : min(odd[l+r-i], r-i+1);
-        while (0 <= i-k and i+k < n and s[i-k] == s[i+k])
-            k++;
-        odd[i] = k--;
-        if (i+k > r)
-            l = i-k, r = i+k;
-    }
-    for(i = 0, l = 0, r = -1; i < n; i++) {
-        int k = (i > r) ? 0 : min(even[l+r-i+1], r-i+1);
-        while (0 <= i-k-1 and i+k < n and s[i-k-1] == s[i+k])
-            k++;
-        even[i] = k--;
-        if (i+k > r)
-            l = i-k-1, r = i+k;
-    }
-    for(i = 0; i < n; i++) 
-        even[i] *= 2;
-};
+/* Palindromic substrings using Manacher's algorithm */
+// (https://cp-algorithms.com/string/manacher.html)
 
+auto manachers = [&] (const string& s) {
+// a b c b a a b b
+// 0 0 0 0 0 2 0 1  -- even length stores len/2
+// 1 1 3 1 1 1 1 1  -- odd  length stores (len+1)/2
+    int n = s.size();
+    array<vector<int>, 2> res;
+    vector<int>& even = res[0];
+    vector<int>& odd = res[1];
+    odd.resize(n); even.resize(n);
+    for(int z = 0, l = 0, r = -1; z < n; z++) {
+        int k = (z > r) ? 1 : min(odd[l+r-z], r-z+1);
+        while(0 <= z-k and z+k < n and s[z-k] == s[z+k]) k++;
+        odd[z] = k--;
+        if(z+k > r) l = z-k, r = z+k;
+    } for(int z = 0, l = 0, r = -1; z < n; z++) {
+        int k = (z > r) ? 0 : min(even[l+r-z+1], r-z+1);
+        while(0 <= z-k-1 and z+k < n and s[z-k-1] == s[z+k]) k++;
+        even[z] = k--;
+        if(z+k > r) l = z-k-1, r = z+k;
+    } return res;
+};
