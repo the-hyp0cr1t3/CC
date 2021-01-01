da610ba99fef5fe5c7d32ac0a474a3a651170350
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 00:07:56 +0530

    Update Binary search.cpp

--- 末 Snippets/Binary search.cpp
+++ 末 Snippets/Binary search.cpp
@@ -5,12 +5,9 @@ auto check = [&] (int x) {
 
 auto binarySearch = [&] (int low, int high) {
     while(low <= high) {
-        int mid = (low + high) / 2;
-        if(check(mid))
-            low = mid + 1;
-        else
-            high = mid - 1;
-    }
-    return high; 
+        int mid = low + high >> 1;
+        if(check(mid)) low = mid + 1;       // Assuming TT...TFF...F
+        else high = mid - 1;
+    } return high; // return low for first occurrence of False, high for last occurrence of True
 };
 
