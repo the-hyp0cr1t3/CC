d541d9057330284d676883606aea3b9ddefd16a0
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 02:41:38 +0530

    Update SCC.cpp

--- 末 Snippets/SCC.cpp
+++ 末 Snippets/SCC.cpp
@@ -1,87 +1,62 @@
 /* Strongly Connected Components */
+// (https://www.youtube.com/watch?v=z9oOadBgO9I&ab_channel=AlgorithmsLive)
 
 // Kosaraju's algorithm (https://cp-algorithms.com/graph/strongly-connected-components.html)
-int n, m, sccount = 0;
-vector<int> g[N], rg[N], comp;
-vector<bool> vis;
+int sccount = 0;
+vector<vector<int>> g(n), rg(n);
+vector<int> comp(n);
+vector<bool> vis(n);
 stack<int> st;
 
-void makeG() {
-    for(int i = 0; i < m; i++) {
-        int u, v;
-        re(u, v);
-        u--, v--;
-        g[u].pb(v);
-        rg[v].pb(u);
-    }
-    vis.resize(n);
-    comp.resize(n);
+for(int i = 0; i < m; i++) {
+    int u, v; cin >> u >> v; u--, v--;
+    g[u].push_back(v); rg[v].push_back(u);
 }
 
-void dfs1(int v) {
+function<void(int)> dfs1 = [&](int v) {
     vis[v] = 1;
     for(auto x: g[v]) 
         if(!vis[x]) dfs1(x);
     st.push(v);
-}
+};
 
-void dfs2(int v, int k) {
+function<void(int)> dfs2 = [&](int v, int k) {
     comp[v] = k;
     for(auto& x: rg[v]) 
         if(!comp[x]) dfs2(x, k);
-}
+};
 
-// Kosaraju's SCC
-void scc() {
-    for(i = 0; i < n; i++) 
-        if(!vis[i]) dfs1(i);
+for(i = 0; i < n; i++) 
+    if(!vis[i]) dfs1(i);
 
-    while(!st.empty()) {
-        sccount++;
-        dfs2(st.top(), sccount);
-        while(!st.empty() and comp[st.top()]) st.pop();
-    }
+while(!st.empty()) {
+    sccount++;
+    dfs2(st.top(), sccount);
+    while(!st.empty() and comp[st.top()]) st.pop();
 }
 
 /* -------------------------------------------------- */
 // Tarjan's algorithm
-int n, m, id = 0, sccount = 0;
-vector<int> g[N], ids, low;
-vector<bool> onstack;
+vector<int> g[N];
+
+int id = 0, sccount = 0;
+vector<int> ids(n, -1), low(n);
+vector<bool> onstack(n);
 stack<int> st;
 
-void makeG() {
-    for(int i = 0; i < m; i++) {
-        int u, v;
-        re(u, v);
-        u--; v--;
-        g[u].pb(v);
-    }
-    ids.assign(n, -1);
-    low.rsz(n);
-    onstack.rsz(n);
-}
-
 // Tarjan's SCC
-void dfs(int v) {
+function<void(int)> dfs = [&](int v) {
     ids[v] = low[v] = id++;
-    onstack[v] = 1;
-    st.push(v);
+    onstack[v] = 1; st.push(v);
     for(auto& x: g[v]) {
-        if(ids[x] == -1) 
-            dfs(x);
-        if(onstack[x]) 
-            low[v] = min(low[v], low[x]);
+        if(ids[x] == -1) dfs(x);
+        if(onstack[x]) low[v] = min(low[v], low[x]);
     }
     if(low[v] == ids[v]) {
         while(!st.empty() and st.top() != v) {
-            int x = st.top();
-            onstack[x] = 0;
-            st.pop();
-        }
-        onstack[v] = 0;
-        st.pop();
-        sccount++;
+            onstack[st.top()] = 0; st.pop();
+        } onstack[v] = 0; st.pop(); sccount++;
     }
 };
 
+for(i = 0; i < n; i++) dfs(i);
