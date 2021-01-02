cf914bb183e1134c92b96f0a6fb078c65c9606d8
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 12:12:56 +0530

    Update DSU.cpp

--- 末 Snippets/DSU.cpp
+++ 末 Snippets/DSU.cpp
@@ -32,3 +32,21 @@ struct DSU {
         } assert(cnt == components);
     }
 };
+
+
+// lite ver ------------------------------
+vector<int> par(n+1, -1);
+auto get_par = [&](int A) {
+    while(par[A] >= 0) {
+        if(par[par[A]] >= 0) par[A] = par[par[A]];
+        A = par[A];
+    } return A;
+};
+
+auto merge = [&](int A, int B) {
+    A = get_par(A); B = get_par(B);
+    if(A == B) return false;
+    if(-par[A] < -par[B]) swap(A, B);
+    par[A] += par[B]; par[B] = A;
+    return true;
+};
