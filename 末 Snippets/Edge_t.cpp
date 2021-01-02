1b1ef39f5e927f4acb9c3a80226046b7a57dcdd6
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-02 12:10:07 +0530

    Update Edge_t.cpp

--- 末 Snippets/Edge_t.cpp
+++ 末 Snippets/Edge_t.cpp
@@ -1,8 +1,13 @@
 struct edge_t {
-    int u, v, w;
+    int u{-1}, v{-1}; int64_t w{0};
     edge_t() = default;
-    edge_t(int u, int v, int w) : u(u), v(v), w(w) {}
+    edge_t(int u, int v, int64_t w = 0) : u(u), v(v), w(w) {}
     bool operator<(const edge_t& other) { return w < other.w; }
     friend ostream& operator<<(ostream& os, const edge_t& e) { os << e.u << ' ' << e.v << ' ' << e.w; return os; }
 };
 
+/* vector<edge_t> edges;
+   edges.emplace_back(2, 3, 123456);
+   edges.emplace_back(1, 2, 134);
+   sort(edges.begin(), edges.end());
+*/
