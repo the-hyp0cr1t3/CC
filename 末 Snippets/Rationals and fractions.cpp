ae553c956f58289c52891eea1e9a4354789a0307
Hriday G <55075129+the-hyp0cr1t3@users.noreply.github.com>
2021-01-07 00:45:02 +0530

    Update Rationals and fractions.cpp

--- 末 Snippets/Rationals and fractions.cpp
+++ 末 Snippets/Rationals and fractions.cpp
@@ -20,9 +20,9 @@ struct frac {
         }
     }
 
-    friend const T floor(const frac& r) { return r.num / r.den - ((r.num ^ r.den) < 0 and r.num % r.den); }
-    friend const T ceil(const frac& r) { return r.num / r.den + ((r.num ^ r.den) > 0 and r.num % r.den); }
-    friend const frac abs(const frac& r) { frac ret = r; if(ret.num < 0) ret.num = -ret.num; return ret; }
+    friend T floor(const frac& r) { return r.num / r.den - ((r.num ^ r.den) < 0 and r.num % r.den); }
+    friend T ceil(const frac& r) { return r.num / r.den + ((r.num ^ r.den) > 0 and r.num % r.den); }
+    friend frac abs(const frac& r) { frac ret = r; if(ret.num < 0) ret.num = -ret.num; return ret; }
 
     int compare(const frac& o) const noexcept {
         if(den == 0 and o.den == 0) return (num ^ o.num) > 0? 0 : num < 0? -1 : 1;
@@ -39,23 +39,23 @@ struct frac {
     bool operator>=(const frac& o) const noexcept { return compare(o) >= 0; }
     bool operator!=(const frac& o) const noexcept { return compare(o) != 0; }
 
-    const frac operator-() const { return frac(-num, den); }
+    frac operator-() const { return frac(-num, den); }
 
     frac& operator+=(const frac& o) { 
         num = num * o.den + den * o.num; den *= o.den; reduce(); return *this;
-    } const frac operator+(const frac& o) const { frac ret = *this; ret += o; return ret; }
+    } frac operator+(const frac& o) const { frac ret = *this; ret += o; return ret; }
 
     frac& operator-=(const frac& o) { 
         num = num * o.den - den * o.num; den *= o.den; reduce(); return *this;
-    } const frac operator-(const frac& o) const { frac ret = *this; ret -= o; return ret; }
+    } frac operator-(const frac& o) const { frac ret = *this; ret -= o; return ret; }
 
     frac& operator*=(const frac& o) {
         num *= o.num; den *= o.den; reduce(); return *this;
-    } const frac operator*(const frac& o) const { frac ret = *this; ret *= o; return ret; }
+    } frac operator*(const frac& o) const { frac ret = *this; ret *= o; return ret; }
 
     frac& operator/=(const frac& o) {
         num *= o.den; den *= o.num; reduce(); return *this;
-    } const frac operator/(const frac& o) const { frac ret = *this; ret /= o; return ret; }
+    } frac operator/(const frac& o) const { frac ret = *this; ret /= o; return ret; }
 
 };
 
